667ee1bee9d0edabb198d74610b925cc5a49b65e
azadb5032-maker <azadb5032@gmail.com>
2026-07-17 16:46:59 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,11 @@
-
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🚀 به سامانه ثبت‌نام سرویس VPN خوش آمدید.\n\n"
+        "🎁 تست رایگان ۱ گیگابایتی برای کاربران جدید فعال است.\n\n"
+        "📱 جهت ادامه، لطفاً شماره تماس خود را از طریق دکمه زیر ارسال نمایید."
+    )
